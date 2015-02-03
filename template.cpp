4517bb90ea313c3abc96f1cb656180ec2581feb7
Subash Chandra <subash.k3110@outlook.com>
2015-02-03 22:38:50 +0530

    added assig2

---
+++ template.cpp
@@ -0,0 +1,106 @@
+
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+
+
+using namespace std;
+
+struct node
+{
+	int data;
+	struct node *next;
+	struct node *arbitptr;
+};
+
+typedef struct node Listnode;
+typedef Listnode *Listnodeptr;
+
+void printList(Listnodeptr startptr)
+{
+	if(startptr)
+	{
+		while(startptr)
+		{
+			printf("%d-->",startptr->data);
+			startptr = startptr->next;
+		}
+		printf("NULL\n\n");
+	}
+	else
+	{
+		printf("list is empty\n");
+	}
+}
+
+void insert(Listnodeptr *startptr, int data)
+{
+	Listnodeptr newnode = (Listnode *)malloc(sizeof(Listnode));
+	Listnodeptr preptr=NULL;
+	Listnodeptr currentptr = *startptr;
+	if(newnode)
+	{
+		newnode->data = data;
+		newnode->next = NULL;
+
+		while(currentptr != NULL)//iterate to the end
+		{
+			preptr = currentptr;
+			currentptr = currentptr->next;
+		}
+
+		if(preptr == NULL)//if list is empty
+		{
+			*startptr = newnode;
+		}
+		else //else insert at the end of the list
+		{
+			preptr->next = newnode;
+		}
+	}
+	else
+	{
+		printf("error: out of memory\n");
+	}
+}
+
+//delete func
+
+void deleteList(Listnodeptr *startptr)
+{
+	Listnodeptr temp;
+
+	while(*startptr != NULL)
+	{
+		temp= *startptr;
+		*startptr = (*startptr)->next;
+		printf("%d deleted\n",temp->data);
+		free(temp);
+	}
+}
+
+
+
+int main()
+{
+	Listnodeptr startptr = NULL;
+	int n, data, i,nValue;
+	int hasLoop = 0;//false
+
+	printf("enter the no of elements in the list: ");
+	scanf("%d",&n);
+
+	printf("enter all the elements: ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&data);
+		insert(&startptr,data);
+	}
+
+
+	printList(startptr);
+
+
+	return 0;
+}
+
